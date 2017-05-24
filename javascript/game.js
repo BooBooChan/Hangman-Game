544d7f2e57a101ac735249d23a429e070dd28fd0
Bridget Bourne <Bridget Bourne>
2017-05-23 22:09:32 -0400

    Basic outline and notes for javascript coding

---
+++ javascript/game.js
@@ -0,0 +1,31 @@
+
+var 
+
+/*Create a function which retrieves user's letter input and displays it accordingly on the page.
+This will be in the Letters Already Guessed Section.*/
+
+/*Create a function which displays correct letters and substitutes corresponding underscores with them.*/
+
+
+
+
+for(){
+
+};
+
+/*Create a condition that will determine a right or wrong letter after user inputs 1 letter with a key stroke.*/
+
+if(){
+
+}; else{
+
+};
+//Create a condition that will determine whether player won or lost.
+
+
+//Some elements you will need to apply above.
+"Press any key to start this glamorous game!"
+"Number of Guesses Remaining:"
+"Letters Already Guessed:"
+"Congratulations, you won! Want to play again?"
+"Nice try. Want to play again?"
