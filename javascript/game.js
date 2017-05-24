aae21c0caff2702f125261296409045aa7003e2e
Bridget Bourne <Bridget Bourne>
2017-05-23 23:41:57 -0400

    Some progress questions and notes taken with beginning steps to create game

--- javascript/game.js
+++ javascript/game.js
@@ -1,10 +1,23 @@
 
-var 
 
-/*Create a function which retrieves user's letter input and displays it accordingly on the page.
-This will be in the Letters Already Guessed Section.*/
 
-/*Create a function which displays correct letters and substitutes corresponding underscores with them.*/
+/*Step 1: Create a function which retrieves user's letter input and displays it accordingly on the page.
+This will be in the Letters Already Guessed Section.
+You will probably need to write an event*/
+var letterGuess = ["a", "b", "c" "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var correctWord =
+var guessesLeft =
+var guessesUsed =
+
+/*So the function name inserted after function can by anything you like,
+because it is a local ___. The element in parentheses is the variable input being applied to the function.*/
+function letterGuessed (letterGuess) { return
+
+}
+
+/*What kind of function do I need to do this?
+
+/*Step 2: Create a function which displays correct letters and substitutes corresponding underscores with them.*/
 
 
 
@@ -13,14 +26,14 @@ for(){
 
 };
 
-/*Create a condition that will determine a right or wrong letter after user inputs 1 letter with a key stroke.*/
+/*Step 3: Create a condition that will determine a right or wrong letter after user inputs 1 letter with a key stroke.*/
 
 if(){
 
 }; else{
 
 };
-//Create a condition that will determine whether player won or lost.
+// Step 4: Create a condition that will determine whether player won or lost.
 
 
 //Some elements you will need to apply above.
