1c783a11a061cf6adc243666e48041db16371a50
Bridget Bourne <bbourne8891@hotmail.com>
2018-02-11 23:19:40 -0500

    It looks like theres a problem with the browser/text editor rather than the code. After eliminating dozens of possibilities behind faulty code, that seems to be the only option left. Maybe an update is necessary? I'll try with a couple other projects first.

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,17 @@
+var peaches = alert('Peaches');
+if(peaches){
+	alert('some progress is made');
+	console.log("progress");
+};
+
+
 // alert("Peaches");
 // document.getElementsByTagName('input').addEventListener("keypress", alert("Peaches"));
 //The alert is appearing after a page refresh, not after a key press as intended.
 //So code recognizes addEventListener() as a method, it's just not executing the method. The effect is exactly the same as alert("Peaches") all by itself.
 //HTML page seems to process the event better than JS page. It accepted the JS code inside the HTML tag.
 //JS page doesn't recognize id name enter-letter. Nor does it recognize the addEventListener method. Moving script from body to head hasn't helped.
+
+//Just adding a conditional has caused alert("Peaches") to appear upon page refresh.
+//Conditional did not execute. Nothing was added to console and no new alerts arose.
+//Defining the alert as a variable changed nothing.
